84694279ea2f85459902598cdf7e33c84fb07f3a
Max McDonnell <max.t.mcdonnell@gmail.com>
2024-05-18 17:18:55 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+export { EditorView, basicSetup } from 'codemirror';
+export { javascript } from '@codemirror/lang-javascript';
+export { autocompletion } from '@codemirror/autocomplete';
+export {
+  createDefaultMapFromCDN,
+  createSystem,
+  createVirtualTypeScriptEnvironment,
+} from '@typescript/vfs';
+export ts from 'typescript';
+export {
+  tsLinter,
+  tsHover,
+  tsAutocomplete,
+  tsSync,
+} from '@valtown/codemirror-ts';
+
+
